54068b448372388bec07c016ab8231cd215671b5
robmikh <rob.mikh@outlook.com>
2022-07-30 13:52:09 -0700

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,30 @@
-use std::{fs::File, path::Path, io::{Write, Read}};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
-use sha2::{Sha512, Digest};
-use xml::{EventReader, reader::XmlEvent};
+use sha2::{Digest, Sha512};
+use xml::{reader::XmlEvent, EventReader};
 
-pub fn download_package_bytes(package_name: &str, version: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn download_package_bytes(
+    package_name: &str,
+    version: &str,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let url = format!("https://www.nuget.org/api/v2/package/{package_name}/{version}");
-    let bytes: Vec<u8> = reqwest::blocking::get(url)?.bytes()?.iter().map(|x| *x).collect();
+    let bytes: Vec<u8> = reqwest::blocking::get(url)?
+        .bytes()?
+        .iter()
+        .map(|x| *x)
+        .collect();
     Ok(bytes)
 }
 
-pub fn download_package_overwrite<P: AsRef<Path>>(package_name: &str, version: &str, download_dir: P) -> Result<File, Box<dyn std::error::Error>> {
+pub fn download_package_overwrite<P: AsRef<Path>>(
+    package_name: &str,
+    version: &str,
+    download_dir: P,
+) -> Result<File, Box<dyn std::error::Error>> {
     let download_dir = download_dir.as_ref();
     let bytes = download_package_bytes(package_name, version)?;
     std::fs::create_dir_all(download_dir)?;
@@ -27,9 +42,13 @@ pub fn download_package_overwrite<P: AsRef<Path>>(package_name: &str, version: &
     Ok(file)
 }
 
-pub fn download_package<P: AsRef<Path>>(package_name: &str, version: &str, download_dir: P) -> Result<File, Box<dyn std::error::Error>> {
+pub fn download_package<P: AsRef<Path>>(
+    package_name: &str,
+    version: &str,
+    download_dir: P,
+) -> Result<File, Box<dyn std::error::Error>> {
     let download_dir = download_dir.as_ref();
-    
+
     // Get the download file path
     let package_file_name = get_package_file_name(package_name, version);
     let path = {
@@ -44,7 +63,7 @@ pub fn download_package<P: AsRef<Path>>(package_name: &str, version: &str, downl
     } else {
         false
     };
-    
+
     let file = if !matches {
         download_package_overwrite(package_name, version, download_dir)?
     } else {
@@ -57,7 +76,11 @@ fn get_package_file_name(package_name: &str, version: &str) -> String {
     format!("{package_name}.{version}.nupkg")
 }
 
-fn package_matches_hash<P: AsRef<Path>>(package_name: &str, version: &str, package_file: P) -> Result<bool, Box<dyn std::error::Error>> {
+fn package_matches_hash<P: AsRef<Path>>(
+    package_name: &str,
+    version: &str,
+    package_file: P,
+) -> Result<bool, Box<dyn std::error::Error>> {
     // Get the hash from nuget.org
     let hash = get_package_hash(package_name, version)?;
     let reference_hash = base64::decode(&hash.hash)?;
@@ -77,7 +100,7 @@ fn package_matches_hash<P: AsRef<Path>>(package_name: &str, version: &str, packa
     file.read_to_end(&mut bytes)?;
     hasher.update(&bytes);
     let file_hash = hasher.finalize();
-    
+
     // Compare the hashes
     let reference_iter = reference_hash.iter();
     let actual_iter = file_hash.iter();
@@ -102,16 +125,18 @@ pub enum HashAlgorithm {
 impl HashAlgorithm {
     pub fn from_string(string: String) -> Self {
         match string.as_str() {
-            "SHA512" | "sha512" => {
-                Self::SHA512
-            }
-            _ => Self::Unknown(string)
+            "SHA512" | "sha512" => Self::SHA512,
+            _ => Self::Unknown(string),
         }
     }
 }
 
-pub fn get_package_hash(package_name: &str, version: &str) -> Result<PackageHash, Box<dyn std::error::Error>> {
-    let url = format!("https://www.nuget.org/api/v2/Packages(Id='{package_name}',Version='{version}')");
+pub fn get_package_hash(
+    package_name: &str,
+    version: &str,
+) -> Result<PackageHash, Box<dyn std::error::Error>> {
+    let url =
+        format!("https://www.nuget.org/api/v2/Packages(Id='{package_name}',Version='{version}')");
     let text = reqwest::blocking::get(url)?.text()?;
 
     let parser = EventReader::from_str(&text);
@@ -120,25 +145,25 @@ pub fn get_package_hash(package_name: &str, version: &str) -> Result<PackageHash
     let mut package_hash_algorithm = None;
     while let Some(e) = event_iter.next() {
         match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                match name.local_name.as_str() {
-                    "PackageHash" => {
-                        let next_text = get_text(event_iter.next().unwrap().unwrap()).unwrap();
-                        package_hash = Some(next_text);
-                    }
-                    "PackageHashAlgorithm" => {
-                        let next_text = get_text(event_iter.next().unwrap().unwrap()).unwrap();
-                        package_hash_algorithm = Some(next_text);
-                    }
-                    _ => {}
+            Ok(XmlEvent::StartElement { name, .. }) => match name.local_name.as_str() {
+                "PackageHash" => {
+                    let next_text = get_text(event_iter.next().unwrap().unwrap()).unwrap();
+                    package_hash = Some(next_text);
                 }
+                "PackageHashAlgorithm" => {
+                    let next_text = get_text(event_iter.next().unwrap().unwrap()).unwrap();
+                    package_hash_algorithm = Some(next_text);
+                }
+                _ => {}
             },
             _ => {}
         }
     }
 
     let package_hash = package_hash.expect("Package hash not found!");
-    let package_hash_algorithm = HashAlgorithm::from_string(package_hash_algorithm.expect("Package hash algorithm not found!"));
+    let package_hash_algorithm = HashAlgorithm::from_string(
+        package_hash_algorithm.expect("Package hash algorithm not found!"),
+    );
 
     Ok(PackageHash {
         hash: package_hash,
@@ -149,6 +174,6 @@ pub fn get_package_hash(package_name: &str, version: &str) -> Result<PackageHash
 fn get_text(event: XmlEvent) -> Option<String> {
     match event {
         XmlEvent::Characters(string) => Some(string),
-        _ => None
+        _ => None,
     }
 }
